9db9875c3f37c7df320790f6fb48aaa746040cf1
Aman <amanhongal@gmail.com>
2024-07-25 14:08:21 +0530

    basic of function is cleared!

--- Js/jslevel2/practice.js
+++ Js/jslevel2/practice.js
@@ -5,3 +5,25 @@ function helloYou(name){
 function addNum(num1,num2){
     console.log(num1+num2);
 }
+
+function names(name = "Aman"){
+    console.log(name);
+}
+
+function formal(name="Sam", title="Sir"){
+    return title+" "+name
+}
+
+//scope
+
+var v = "Global V"
+var stuff = "Global Staff"
+
+function fun(stuff){
+    console.log(v);
+    stuff = "Reassign stuff inside fun"
+    console.log(stuff);
+}
+
+fun()
+console.log(stuff);
