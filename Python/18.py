7aabb18318e64a4b19878b2e5efb663d7229047e
Aman <amanhongal@gmail.com>
2024-08-07 12:12:02 +0530

    __name__ and __main__ is cleared and python is completed

---
+++ Python/18.py
@@ -0,0 +1,8 @@
+import amans_module
+print("TOP level 18.py")
+amans_module.func()
+
+if __name__ == '__main__':
+    print("18.py is being run directly")
+else:
+    print("18.py is been imported")
--- Python/amans_module.py
+++ Python/amans_module.py
@@ -1,4 +1,14 @@
 
 
-def func_in_module():
-    print("I am inside aman's module")
+# def func_in_module():
+#     print("I am inside aman's module")
+
+def func():
+    print("func() in amans_module.py")
+
+print("TOP level amans_module.py")
+
+if __name__ == '__main__':
+    print("amans_module.py is being run directly")
+else:
+    print("Amans_module.py is been imported")
