e44808fdba247b3d9885ab2053701bc348f136a1
Aman <amanhongal@gmail.com>
2024-08-07 11:53:13 +0530

    Modules and how to import them is studied

---
+++ Python/17_1.py
@@ -0,0 +1,5 @@
+# from amans_module import func_in_module
+# from amans_module import * # this will import everything from that file
+import amans_module
+
+amans_module.func_in_module()
---
+++ Python/amans_module.py
@@ -0,0 +1,4 @@
+
+
+def func_in_module():
+    print("I am inside aman's module")
