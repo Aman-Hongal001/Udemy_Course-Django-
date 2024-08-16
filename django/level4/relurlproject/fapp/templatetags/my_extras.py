6ae8e1673bb3b2aa98f21c1619c866d71813b78f
Aman <amanhongal@gmail.com>
2024-08-16 22:31:47 +0530

    Registeration methods with decorators was done

--- django/level4/relurlproject/fapp/templatetags/my_extras.py
+++ django/level4/relurlproject/fapp/templatetags/my_extras.py
@@ -1,7 +1,8 @@
 from django import template
 
 register = template.Library()
-
+# registeration using decorators
+@register.filter(name='cut')
 def cut(value,arg):
     """
     this cuts out all various of "arg" from the string!
